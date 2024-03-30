ed90f7a22676092afcb47edca42bf707cf765106
Terrance Yu-Hao Chen <yhterrance@csie.ntu.edu.tw>
2024-03-30 16:13:58 +0800

    Fix relative path issue in abi.ts

--- apps/telegram-bot/lib/utils/abi.ts
+++ apps/telegram-bot/lib/utils/abi.ts
@@ -1,5 +1,5 @@
 import fs from "fs";
 
 export const wagmiAbi = JSON.parse(
-  fs.readFileSync("./abi/XueDAOCoreContributor.abi", "utf8"),
+  fs.readFileSync(`${__dirname}/../../abi/XueDAOCoreContributor.abi`, "utf8")
 );
